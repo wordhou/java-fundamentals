9cdb82efc7fce4bed3259a6c0fa5d1266bafaa7a
Ed <houedward@gmail.com>
2021-03-24 08:15:07 -0700

    initiated gradle project for labs, completed arrays, loop, and testing labs

--- README.md
+++ README.md
@@ -17,3 +17,23 @@ The `flipNHeads` method simulates a random trial where coins are flipped until a
 ## clock
 
 The `clock` method prints the current time to the console every second. This method executes in an infinite loop and only stops when the program is killed (`Ctrl+C`).
+
+# Lab 02: Arrays, Loops, and Testing
+
+The methods were written in the `ArrayLoops` class.
+
+## roll
+
+Our `roll` method takes an `int n` returns an array of `n` die rolls, where each die roll is a random integer from 1 through 6.
+
+## containsDuplicates
+
+The `containsDuplicates` method takes an `int` array as input and returns a boolean indicating whether or not there are any duplicates in the array.
+
+## getAverage
+
+The `getAverage` method takes an `int` array and returns a double with the average value of the array.
+
+## getAverageOfAverages
+
+The `getAverageOfAverages` method takes an array of array of `int`s and returns the average of the average values of each array.
---
+++ basiclibrary/lib/src/main/java/basiclibrary/ArraysLoops.java
@@ -0,0 +1,49 @@
+package basiclibrary;
+
+import java.util.Random;
+
+public class ArraysLoops {
+    public static int[] roll(int numberOfRolls) {
+        Random rand = new Random();
+        int[] result = new int[numberOfRolls];
+
+        for (int i = 0; i < numberOfRolls; i++) {
+            result[i] = rand.nextInt(6) + 1;
+        }
+
+        return result;
+    }
+
+    public static boolean containsDuplicates(int[] array) {
+        // We are comparing every the element in the array with every element that
+        // comes after it in the array, because every element has already been compared
+        // to the element before it.
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] == array[j]) return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static double getAverage(int[] array) {
+        if (array.length == 0) throw new IndexOutOfBoundsException("Array can't be empty");
+        double sum = 0;
+        for (int value : array) {
+            sum = sum + value;
+        }
+
+        return sum / array.length;
+    }
+
+    public static double getAverageOfAverages(int[][] arrayOfArrays) {
+        if (arrayOfArrays.length == 0) throw new IndexOutOfBoundsException("Array can't be empty");
+        double sum = 0;
+        for (int[] array : arrayOfArrays) {
+            sum = sum + getAverage(array);
+        }
+
+        return sum / arrayOfArrays.length;
+    }
+}
